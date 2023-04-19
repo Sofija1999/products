b6132652cbcd310da8c44a280686062764469544
Sofija1999 <sofija.dokmanovic99@gmail.com>
2023-04-19 19:57:25 +0200

    product struct created

--- models/models.go
+++ models/models.go
@@ -1,5 +1,13 @@
 package models
 
-import(
-	
-)
+import "time"
+
+type Product struct {
+	id int64 `json:"id"`
+	price float64 `json:"price"`
+	shortDescription string `json:"shortDescription"`
+	description string `json:"description"`
+	created time.Time `json:"created"`
+	updated time.Time `json:"updated"`
+	name string `json:"name"`
+}

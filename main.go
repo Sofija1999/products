e114eb192c51ad2fec834473b302420349503f7d
Sofija1999 <sofija.dokmanovic99@gmail.com>
2023-04-19 19:39:27 +0200

    init router, route handlers

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import(
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"products/routers"
 
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
+	r := routers.Router()
+	fmt.Println("Starting server on the port 8080..")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 	
 }
--- routers/routers.go
+++ routers/routers.go
@@ -1,5 +1,21 @@
 package routers
 
-import(
-	
+import (
+	"products/middleware"
+
+	"github.com/gorilla/mux"
 )
+
+func Router() *mux.Router{
+	//Init router
+	router := mux.NewRouter()
+
+	//Route Handlers
+	router.HandleFunc("/api/product/{id}", middleware.GetProduct).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/product", middleware.GetAllProducts).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/newproduct", middleware.CreateProduct).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/product/{id}", middleware.UpdateProduct).Methods("PUT", "OPTIONS")
+	router.HandleFunc("/api/deleteproduct/{id}", middleware.DeleteProduct).Methods("DELETE", "OPTIONS")
+
+	return router
+}

6784ecfa109006d442cec9620fb4b11b0dd84449
Sofija1999 <sofija.dokmanovic99@gmail.com>
2023-05-12 14:23:18 +0200

    create model user

--- models/models.go
+++ models/models.go
@@ -20,3 +20,12 @@ type Category struct {
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
 }
+
+type User struct {
+	Id int64 `json:"id"`
+	First_name string `json:"first_name"`
+	Last_name string `json:"last_name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+	Created_at time.Time `json:"created_at"`
+}
